365e9cf268d0741cf153b60bb5f4f6b50a938374
vanessateixeira92 <vanessa.r.m.teixeira@gmail.com>
2024-06-30 15:47:52 +0100

    Added the basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateRecipes(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipes", {
+    strings: "Banana Bread",
+    autoStart: true,
+    cursor: null,
+    delay: 4,
+  });
+}
+console.log("Generating recipes..");
+
+let formElement = document.querySelector("#generate-recipes");
+formElement.addEventListener("submit", generateRecipes);
